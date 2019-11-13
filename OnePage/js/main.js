e20867cd8efb1b152d6349ab84291ba2ca6bcf5e
CPrimbee <cprimbee@gmail.com>
2019-11-13 16:01:54 -0300

    Aula 73 a 87

---
+++ OnePage/js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+    "use strict";
+    // Inicializar nosso contador
+    $('.contar').counterUp({
+        time: 1000,
+        delay: 10
+    });
+
+    let $doc = $('html,body');
+    $('.scroll-page').click(function () {
+        $doc.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+
+})(jQuery);
